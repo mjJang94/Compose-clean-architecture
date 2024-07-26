d51796d0aa0071e3b70138ac2746854f6489afc0
mjJang94 <alswhddl10@naver.com>
2024-07-26 19:07:58 +0900

    NewsPagingSource 사용처의 inner class로 정의

--- presentation/home/src/main/java/com/mj/presentation/home/HomeViewModel.kt
+++ presentation/home/src/main/java/com/mj/presentation/home/HomeViewModel.kt
@@ -5,8 +5,11 @@ import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
 import androidx.paging.cachedIn
 import com.mj.core.base.BaseViewModel
+import com.mj.domain.model.News
 import com.mj.domain.usecase.GetNewsUseCase
 import com.mj.domain.usecase.GetNewsUseCase.GetNewsParam as Param
 import com.mj.presentation.home.model.NewsInfo
@@ -65,6 +68,44 @@ class HomeViewModel @Inject constructor(
                     setEffect { HomeContract.Effect.DataLoaded }
                 }
         }
+    }
+
+    private inner class NewsPagingSource(
+        private val load: suspend (query: String, index: Int) -> News,
+        private val query: String,
+    ) : PagingSource<Int, NewsInfo.Content>() {
+
+        override suspend fun load(params: LoadParams<Int>): LoadResult<Int, NewsInfo.Content> {
+            return try {
+                val currentPage = params.key ?: 1
+                val news = load.invoke(query, currentPage).translate()
+
+                LoadResult.Page(
+                    data = news.contents,
+                    prevKey = if (currentPage == 1) null else currentPage - 1,
+                    nextKey = news.currentPage + 1
+                )
+            } catch (exception: Exception) {
+                return LoadResult.Error(exception)
+            }
+        }
+
+        override fun getRefreshKey(state: PagingState<Int, NewsInfo.Content>): Int? {
+            return state.anchorPosition
+        }
+
+        private fun News.translate(): NewsInfo =
+            NewsInfo(
+                currentPage = this.currentPage,
+                contents = this.contents.map { content ->
+                    NewsInfo.Content(
+                        title = content.title,
+                        description = content.description,
+                        date = content.date,
+                        link = content.link,
+                    )
+                }
+            )
     }
 }
 
